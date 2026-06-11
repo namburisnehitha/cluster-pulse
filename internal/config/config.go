49ea14ca144737a48bc81f5b61d6541ac1a74d7e
Snehitha <namburisnehitha@gmail.com>
2026-06-11 14:07:21 +0000

    update docker-compose, add dockerfiles

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,23 +1,28 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"os"
+	"strings"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
-	MySQLDSN                      string
-	RedisAddr                     string
-	RedisPassword                 string
-	SlackWebhookURL               string
-	KafkaBrokers                  []string
-	KafkaTopicName                string
-	GroqAPIKey                    string
-	OpenAIAPIKey                  string
-	GroqModel                     string
-	OpenAIModel                   string
-	JWTSecret                     string
-	WorkerConcurrencyLimit        int
-	ResourceSnapshotRetentionDays int
-	AdminUsername                 string
-	AdminPasswordHash             string
+	MySQLDSN                      string   `mapstructure:"mysql_dsn"`
+	RedisAddr                     string   `mapstructure:"redis_addr"`
+	RedisPassword                 string   `mapstructure:"redis_password"`
+	SlackWebhookURL               string   `mapstructure:"slack_webhook_url"`
+	KafkaBrokers                  []string `mapstructure:"kafka_brokers"`
+	KafkaTopicName                string   `mapstructure:"kafka_topic_name"`
+	GroqAPIKey                    string   `mapstructure:"groq_api_key"`
+	OpenAIAPIKey                  string   `mapstructure:"openai_api_key"`
+	GroqModel                     string   `mapstructure:"groq_model"`
+	OpenAIModel                   string   `mapstructure:"openai_model"`
+	JWTSecret                     string   `mapstructure:"jwt_secret"`
+	AdminUsername                 string   `mapstructure:"admin_username"`
+	AdminPasswordHash             string   `mapstructure:"admin_password_hash"`
+	WorkerConcurrencyLimit        int      `mapstructure:"worker_concurrency_limit"`
+	ResourceSnapshotRetentionDays int      `mapstructure:"resource_snapshot_retention_days"`
 }
 
 func Load(path string) (*Config, error) {
@@ -29,6 +34,11 @@ func Load(path string) (*Config, error) {
 	}
 
 	var cfg Config
+
+	if brokers := os.Getenv("KAFKA_BROKERS"); brokers != "" {
+		cfg.KafkaBrokers = strings.Split(brokers, ",")
+	}
+
 	if err := viper.Unmarshal(&cfg); err != nil {
 		return nil, err
 	}
