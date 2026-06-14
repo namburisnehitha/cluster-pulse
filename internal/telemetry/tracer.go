2de4777763be7f38ebd0c356871e5cb37368ba79
Snehitha <namburisnehitha@gmail.com>
2026-06-14 01:40:03 +0000

    add gettracer func

---
+++ internal/telemetry/tracer.go
@@ -0,0 +1,41 @@
+package telemetry
+
+import (
+	"context"
+	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+)
+
+func InitTracer(serviceName string) (func(), error) {
+
+	res, err := resource.New(context.Background(), resource.WithAttributes(semconv.ServiceName(serviceName)))
+
+	if err != nil {
+		return nil, err
+	}
+
+	exporter, err := otlptracegrpc.New(context.Background(),
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint(os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+	)
+
+	otel.SetTracerProvider(tp)
+
+	return func() {
+		tp.Shutdown(context.Background())
+	}, nil
+}
